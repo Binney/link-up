cdbd9d3bfa6743b9047d3708606fb73080c5f3a3
Binney <sar.binney@hotmail.co.uk>
2013-08-27 17:47:44 +0100

    The sorry fate of Ms Townsend

--- db/migrate/20130814172917_add_attendance_frequency_to_attendings.rb
+++
@@ -1,5 +0,0 @@
-class AddAttendanceFrequencyToAttendings < ActiveRecord::Migration
-  def change
-    add_column :attendings, :repeated, :boolean, default: :false # true = every week, false = one-off
-  end
-end
