45fe909e461a5e904829d690a4ffc7043d8fc113
Binney <sar.binney@hotmail.co.uk>
2013-09-18 14:25:13 +0100

    separate js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,34 +17,6 @@
 //= require jquery.ui.all
 //= require jquery_ujs
 //= require jquery_nested_form
+//= require_self
 
-$(function() {
-  alert("You clicked something!");
-  /* Datepicker on event_start_time (um, obsolete?) */
-  $("#event_start_time").datepicker();
 
-  /* Create tabs. */
-  $("#tabs").tabs({ heightStyle: "auto" });
-
-  /* Hide neweventform on load and allow toggle when you click the toggle button. */
-  $(".neweventform").toggle();
-  $("#toggleform").click(function() {
-    $( ".neweventform" ).slideToggle("slow" );
-  });
-
-  /* AJAX search for users. */
-  $(document).on('submit', '#user_search', function () {
-    $.get(this.action, $(this).serialize(), null, 'script');
-    return false;
-  });
-
-});
-
-/*$(document).on('nested:fieldAdded', function(ev){
-  // this field was just inserted into your form
-  var field = ev.field; 
-  // it's a jQuery object already! Now you can find date input
-  var dateField = field.find('.start_time');
-  // and activate datepicker on it
-  dateField.datepicker();
-})*/
