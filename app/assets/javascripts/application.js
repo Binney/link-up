d2109ff1b9a62790638de39557f63bac4d4bb515
Binney <sar.binney@hotmail.co.uk>
2013-09-18 22:27:47 +0100

    headings for my events tabs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,30 @@
 //= require turbolinks
 //= require bootstrap
 //= require jquery
+//= require jquery_ujs
 //= require jquery.turbolinks
 //= require jquery.ui.all
-//= require jquery_ujs
 //= require jquery_nested_form
 //= require_self
 
+$(function() {
+  alert("You clicked something!");
+  /* Datepicker on event_start_time (um, obsolete?) */
+  $("#event_start_time").datepicker();
+
+  /* Create tabs. */
+  $("#tabs").tabs({ heightStyle: "auto" });
+
+  /* Hide neweventform on load and allow toggle when you click the toggle button. */
+  $(".neweventform").toggle();
+  $("#toggleform").click(function() {
+    $( ".neweventform" ).slideToggle("slow" );
+  });
+
+  /* AJAX search for users. */
+  $(document).on('submit', '#user_search', function () {
+    $.get(this.action, $(this).serialize(), null, 'script');
+    return false;
+  });
+
+});
