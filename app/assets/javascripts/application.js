54964fea40ee218ca8abdd4a106c3aa755bff56e
Binney <sar.binney@hotmail.co.uk>
2013-09-18 14:15:56 +0100

    application js is back

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,33 @@
 //
 //= require turbolinks
 //= require bootstrap
+//= require jquery
+//= require jquery.turbolinks
+//= require jquery.ui.all
+//= require jquery_ujs
+//= require jquery_nested_form
+
+$(function() {
+  alert("You clicked something!");
+  /* Datepicker on event_start_time (um, obsolete?) */
+  $("#event_start_time").datepicker();
+
+  /* Create tabs. */
+  $("#tabs").tabs({ heightStyle: "auto" });
+
+  /* Hide neweventform on load and allow toggle when you click the toggle button. */
+  $(".neweventform").toggle();
+  $("#toggleform").click(function() {
+    $( ".neweventform" ).slideToggle("slow" );
+  });
+
+  /* AJAX search for users. */
+  $(document).on('submit', '#user_search', function () {
+    $.get(this.action, $(this).serialize(), null, 'script');
+    return false;
+  });
+
+});
 
 /*$(document).on('nested:fieldAdded', function(ev){
   // this field was just inserted into your form
