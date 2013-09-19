1360e64cea01449e07c7817b32c89bd30f2a0285
Binney <sar.binney@hotmail.co.uk>
2013-09-19 08:48:47 +0100

    csrf tags ftw

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,24 +20,4 @@
 //= require_self
 //= require_tree .
 
-$(function() {
-  alert("You clicked something!");
-  /* Datepicker on event_start_time (um, obsolete?) */
-  $("#event_start_time").datepicker();
 
-  /* Create tabs. */
-  $("#tabs").tabs({ heightStyle: "auto" });
-
-  /* Hide neweventform on load and allow toggle when you click the toggle button. */
-  $(".neweventform").toggle();
-  $("#toggleform").click(function() {
-    $( ".neweventform" ).slideToggle("slow" );
-  });
-
-  /* AJAX search for users. */
-  $(document).on('submit', '#user_search', function () {
-    $.get(this.action, $(this).serialize(), null, 'script');
-    return false;
-  });
-
-});
