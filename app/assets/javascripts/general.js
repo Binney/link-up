5542e4802ebd6f222ded385d6a7cf205a6ea44e7
Binney <sar.binney@hotmail.co.uk>
2013-09-24 15:13:13 +0100

    with tabs

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -1,5 +1,8 @@
 $( function() {
 
+  /* Create tabs. */
+  $("#tabs").tabs({ heightStyle: "auto" });
+
   /* AJAX search for users. */
   $(document).on('submit', '#user_search', function () {
     $.get(this.action, $(this).serialize(), null, 'script');
