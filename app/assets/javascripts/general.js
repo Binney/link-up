71991b524117aba61076f6b503b8a66649c611b9
Binney <sar.binney@hotmail.co.uk>
2013-09-24 15:22:01 +0100

    with extra js features

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -1,5 +1,12 @@
 $( function() {
 
+  /* Pagination via AJAX */
+  $(document).on("click", '.pagination a', function() {
+    $('.pagination').html('Loading...')
+    $.get(this.href, null, null, "script");
+    return false;
+  });
+
   /* Create tabs. */
   $("#tabs").tabs({ heightStyle: "auto" });
 
@@ -8,5 +15,12 @@ $( function() {
     $.get(this.action, $(this).serialize(), null, 'script');
     return false;
   });
+
+  /* Hide neweventform on load and allow toggle when you click the toggle button. */
+  $(".togglediv").toggle();
+  $("#toggleform").click(function() {
+    $( ".togglediv" ).slideToggle("slow" );
+  });
+
 
 });
