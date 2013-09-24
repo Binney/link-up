b545be2242709c8a844e4b4fb14dbd5958cc4538
Binney <sar.binney@hotmail.co.uk>
2013-09-24 17:34:40 +0100

    gmaps js

--- app/assets/javascripts/pagination.js
+++
@@ -1,8 +0,0 @@
-$( function() {
-  $(document).on("click", '.pagination a', function() {
-    $('.pagination').html('Loading...')
-    $.get(this.href, null, null, "script");
-    return false;
-  });
-});
-
