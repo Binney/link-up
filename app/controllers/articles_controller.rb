a76ea5f3a08804169f08f3e33acec8888ac0dfb9
Binney <sar.binney@hotmail.co.uk>
2013-10-01 20:13:14 +0100

    News system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_filter :not_a_student, only: [:new, :create, :destroy]
 
   def new
     @article = Article.new
@@ -18,14 +19,34 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update_attributes(article_params)
+    redirect_to @article
+  end
+
   def index
     @articles = Article.all.paginate(page: params[:page])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    current_user.articles.find_by(id: @article.id).destroy
+    redirect_to news_path
   end
 
   private
 
     def article_params
       params.require(:article).permit(:title, :content)
+    end
+
+    def not_a_student
+      redirect_to root_path if current_user.role=="student"
     end
 
 end
