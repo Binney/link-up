8ecc4a87118c9cd2f98522168c31da260949bf64
UuOoBb <borzoj@aktivix.org>
2013-12-29 12:29:08 +0000

    disable force ssl in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Sweaton2::Application.configure do
   #config.action_dispatch.x_sendfile_header = nil # for Heroku
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
